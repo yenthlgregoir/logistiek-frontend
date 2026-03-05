16ec616b18159fb1a9b063291293afdc588bd993
Yenth'l Grégoir <yenthl.gregoir@bumaco.be>
2026-03-05 11:47:19 +0100

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // 🔑 Belangrijk voor history mode
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
