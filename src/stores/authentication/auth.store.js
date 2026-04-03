c8857e2b661b1b2ae76fd698b290576458422e0d
Yenth'l Grégoir <yenthl.gregoir@bumaco.be>
2026-04-03 16:29:47 +0200

    auth fix

--- src/stores/authentication/auth.store.js
+++ src/stores/authentication/auth.store.js
@@ -4,10 +4,10 @@ import { loginApi } from '@/api/login.js'
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null,
-    token: localStorage.getItem('token') || null,
+    token: localStorage.getItem('token') || null
   }),
   getters: {
-    isLoggedIn: (state) => !!state.token,
+    isLoggedIn: (state) => !!state.token
   },
   actions: {
     async login(email, password) {
@@ -15,23 +15,23 @@ export const useAuthStore = defineStore('auth', {
       this.token = response.token
       localStorage.setItem('token', this.token)
 
-      await this.fetchMe() // haal user info op na login
+      await this.fetchMe() 
     },
 
     async fetchMe() {
-      if (!this.token) return
-      try {
-        const user = await loginApi.getUsers() // pas hier aan naar jouw endpoint die ingelogde user teruggeeft
-        this.user = Array.isArray(user) ? user[0] : user // als je backend array terugstuurt
-      } catch (err) {
-        this.logout() // token ongeldig? log uit
-      }
-    },
+  try {
+    const res = await loginApi.me()
+    this.user = res
+  } catch (err) {
+    this.user = null
+    this.logout()
+  }
+},
 
     logout() {
       this.user = null
       this.token = null
       localStorage.removeItem('token')
-    },
-  },
+    }
+  }
 })
